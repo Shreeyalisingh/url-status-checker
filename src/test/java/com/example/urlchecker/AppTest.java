77b2166205a53272551dae6597c56801bc88926b
ShreeyaliSingh <your-email@example.com>
2025-06-24 02:51:27 +0530

    Add dummy JUnit test

---
+++ src/test/java/com/example/urlchecker/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.urlchecker;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void sampleTest() {
+        assertTrue(true, "Dummy test should pass");
+    }
+}
+
